1a998e3f816d1b4c4248bbf44c037dce954eb16e
Grigorescu Alexandru <grigorecu.aleex@gmail.com>
2024-03-25 21:28:16 +0000

    feat: finish homework

--- a1/a1.c
+++ a1/a1.c
@@ -16,16 +16,18 @@ typedef enum
 } bool;
 
 void NFSC_List(char **options, int optionsCount);
+void NFSC_Extract(char **options, int optionsCount);
 void NFSC_Parse(char *path);
-void NFSC_DisplayFolderContents(char *path, bool reccursive, char permissions[10], int maxiumumSize);
+void NFSC_FindAll(char *path);
+void NFSC_DisplayFolderContents(char *path, bool reccursive, char permissions[10], int maxiumumSize, bool checkForSFFormat);
 int NFSC_ThrowError(char *message);
 int NSFC_PermissionsToOctal(const char permString[9]);
 char *NFSC_ReverseString(char *str);
-int validateMagicWord(int fd);
 short NFSC_GetHeaderSize(int fd);
-int validateVersion(int fd);
-int validateSectionNumber(int fd);
-int validateSectionTypes(int fd);
+int validateMagicWord(int fd, bool printError);
+int validateVersion(int fd, bool printError);
+int validateSectionNumber(int fd, bool printError);
+int validateSectionTypes(int fd, bool printError);
 
 int main(int argc, char **argv)
 {
@@ -43,8 +45,26 @@ int main(int argc, char **argv)
         {
             NFSC_Parse(argv[2] + 5);
         }
+        if (strcmp(argv[1], "findall") == 0)
+        {
+            NFSC_FindAll(argv[2] + 5);
+        }
+        if (strcmp(argv[1], "extract") == 0)
+        {
+            NFSC_Extract(argv, argc);
+        }
     }
+
     return 0;
+}
+
+void NFSC_FindAll(char *path)
+{
+    puts("SUCCESS");
+
+    char permissions[10] = {0};
+    size_t maximumSize = INT_MAX;
+    NFSC_DisplayFolderContents(path, true, permissions, maximumSize, true);
 }
 
 char *NFSC_ReverseString(char *str)
@@ -70,8 +90,8 @@ void NFSC_Parse(char *path)
     }
     int version, sectionsNumber;
 
-    if (validateMagicWord(fileDescription) || (version = validateVersion(fileDescription)) == 0 ||
-        (sectionsNumber = validateSectionNumber(fileDescription)) == 0 || validateSectionTypes(fileDescription))
+    if (validateMagicWord(fileDescription, true) || (version = validateVersion(fileDescription, true)) == 0 ||
+        (sectionsNumber = validateSectionNumber(fileDescription, true)) == 0 || validateSectionTypes(fileDescription, true))
         return;
 
     puts("SUCCESS");
@@ -92,14 +112,15 @@ void NFSC_Parse(char *path)
     }
 }
 
-int validateMagicWord(int fd)
+int validateMagicWord(int fd, bool printError)
 {
     char magicWord[5] = {0};
     lseek(fd, -4, SEEK_END);
     read(fd, magicWord, 4);
     if (strcmp(magicWord, "Wa49") != 0)
     {
-        printf("ERROR\nwrong magic");
+        if (printError)
+            printf("ERROR\nwrong magic");
         return 1;
     }
 
@@ -116,7 +137,7 @@ short NFSC_GetHeaderSize(int fd)
     return headerSize;
 }
 
-int validateVersion(int fd)
+int validateVersion(int fd, bool printError)
 {
     short headerSize = NFSC_GetHeaderSize(fd);
     int version;
@@ -125,13 +146,14 @@ int validateVersion(int fd)
 
     if ((version < 98) || (version > 127))
     {
-        printf("ERROR\nwrong version");
+        if (printError)
+            printf("ERROR\nwrong version");
         return 0;
     }
     return version;
 }
 
-int validateSectionNumber(int fd)
+int validateSectionNumber(int fd, bool printError)
 {
     short headerSize = NFSC_GetHeaderSize(fd);
     char sectionsNumber;
@@ -139,12 +161,14 @@ int validateSectionNumber(int fd)
     read(fd, &sectionsNumber, 1);
     if (sectionsNumber != 2 && (sectionsNumber < 4 || sectionsNumber > 17))
     {
-        printf("ERROR\nwrong sect_nr");
+        if (printError)
+            printf("ERROR\nwrong sect_nr");
         return 0;
     }
     return sectionsNumber;
 }
-int validateSectionTypes(int fd)
+
+int validateSectionTypes(int fd, bool printError)
 {
     short headerSize = NFSC_GetHeaderSize(fd);
 
@@ -160,12 +184,69 @@ int validateSectionTypes(int fd)
         read(fd, &type, 2);
         if (type != 93 && type != 91 && type != 60)
         {
-            printf("ERROR\nwrong sect_types");
+            if (printError)
+                printf("ERROR\nwrong sect_types");
             return 1;
         }
     }
 
     return 0;
+}
+
+void NFSC_Extract(char **options, int optionsCount)
+{
+    int sectionNumber = 0;
+    int wishedLineNumber = 0;
+    char *path = NULL;
+
+    for (size_t i = 2; i < optionsCount; ++i)
+    {
+        if (strstr(options[i], "path") != NULL)
+        {
+            path = (char *)realloc(path, strlen(options[i]));
+            strcpy(path, options[i] + 5);
+        }
+        else if (strstr(options[i], "section"))
+        {
+            sectionNumber = atoi(options[i] + 8);
+        }
+        else if (strstr(options[i], "line"))
+        {
+            wishedLineNumber = atoi(options[i] + 5);
+        }
+    }
+
+    puts("SUCCESS");
+    int fileDescription = open(path, O_RDONLY);
+    short headerSize = NFSC_GetHeaderSize(fileDescription);
+
+    int sectionOffset;
+    int sectionSize;
+    lseek(fileDescription, -headerSize + 15 + (sectionNumber - 1) * 18, SEEK_END);
+    read(fileDescription, &sectionOffset, 4);
+    read(fileDescription, &sectionSize, 4);
+
+    char section[sectionSize + 1];
+    lseek(fileDescription, sectionOffset, SEEK_SET);
+    read(fileDescription, &section, sectionSize);
+
+    char newLine[] = {10};
+    int lineNumber = 0;
+    char *cur = strtok(section, newLine);
+    while (cur != NULL)
+    {
+        lineNumber++;
+        if (lineNumber == wishedLineNumber)
+        {
+            printf("%s\n", NFSC_ReverseString(cur));
+            break;
+        }
+
+        cur = strtok(NULL, newLine);
+    }
+
+    free(path);
+    path = NULL;
 }
 
 void NFSC_List(char **options, int optionsCount)
@@ -195,13 +276,13 @@ void NFSC_List(char **options, int optionsCount)
     }
 
     puts("SUCCESS");
-    NFSC_DisplayFolderContents(path, recursive, permissions, maximumSize);
+    NFSC_DisplayFolderContents(path, recursive, permissions, maximumSize, false);
 
     free(path);
     path = NULL;
 }
 
-void NFSC_DisplayFolderContents(char *path, bool recursive, char permissions[10], int maximumSize)
+void NFSC_DisplayFolderContents(char *path, bool recursive, char permissions[10], int maximumSize, bool checkForSFFormat)
 {
     DIR *dir = NULL;
     if ((dir = opendir(path)) == NULL)
@@ -230,9 +311,58 @@ void NFSC_DisplayFolderContents(char *path, bool recursive, char permissions[10]
             continue;
 
         if (maximumSize == INT_MAX || S_ISREG(statbuf.st_mode))
-            printf("%s\n", filePath);
+        {
+            if (checkForSFFormat && S_ISREG(statbuf.st_mode))
+            {
+                int fileDescription = open(filePath, O_RDONLY);
+                int sectionsNumber;
+                if (validateMagicWord(fileDescription, false) || validateVersion(fileDescription, false) == 0 ||
+                    (sectionsNumber = validateSectionNumber(fileDescription, false)) < 3 || validateSectionTypes(fileDescription, false))
+                    continue;
+
+                short headerSize = NFSC_GetHeaderSize(fileDescription);
+                int sectionWithLines = 0;
+                for (size_t i = 0; i < sectionsNumber; i++)
+                {
+                    int sectionOffset;
+                    int sectionSize;
+                    lseek(fileDescription, -headerSize + 15 + i * 18, SEEK_END);
+                    read(fileDescription, &sectionOffset, 4);
+                    read(fileDescription, &sectionSize, 4);
+
+                    char section[sectionSize + 1];
+                    lseek(fileDescription, sectionOffset, SEEK_SET);
+                    read(fileDescription, &section, sectionSize);
+
+                    char newLine[] = {0x0d};
+                    int lineNumber = 0;
+                    char *cur = strtok(section, newLine);
+                    while (cur != NULL)
+                    {
+                        lineNumber++;
+                        cur = strtok(NULL, newLine);
+                    }
+
+                    if (lineNumber == 14)
+                    {
+                        sectionWithLines++;
+                        if (sectionWithLines == 3)
+                            break;
+                    }
+                }
+
+                if (sectionWithLines < 3)
+                    continue;
+            }
+
+            if (checkForSFFormat && S_ISREG(statbuf.st_mode))
+                printf("%s\n", filePath);
+            else if (!checkForSFFormat)
+                printf("%s\n", filePath);
+        }
+
         if (recursive && S_ISDIR(statbuf.st_mode))
-            NFSC_DisplayFolderContents(filePath, recursive, permissions, maximumSize);
+            NFSC_DisplayFolderContents(filePath, recursive, permissions, maximumSize, checkForSFFormat);
     }
     closedir(dir);
 }
